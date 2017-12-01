0dc7c527881c42661d4ee0fa65a499ea8b8c56b3
Minglai Liu <33521244+brightcome@users.noreply.github.com>
2017-12-01 11:15:34 +0800

    change for the respberry pi,
    the pyaudio , pygame can't be installed on respberrypi. use os and arecord instead.

--- xiaoq_main.py
+++ xiaoq_main.py
@@ -1,4 +1,14 @@
-#!usr/bin/env python  #coding=utf-8        from aip import AipSpeechimport numpy as np  from pyaudio import PyAudio,paInt16  from datetime import datetime  import wave  from tkinter import *  import urllib.requestimport urllibimport jsonimport pygameimport time
+#!usr/bin/env python  #coding=utf-8        
+from aip import AipSpeech
+import numpy as np  from pyaudio 
+import PyAudio,paInt16  from datetime 
+import datetime 
+import wave  
+from tkinter import *  
+import urllib.requestimport urllib
+import time
+import os
+
 #define of params  NUM_SAMPLES = 2000  framerate = 8000  channels = 1  sampwidth = 2  #record time  TIME = 10
 def get_file_content(filePath):    with open(filePath, 'rb') as fp:        return fp.read()
 def save_wave_file(filename, data):      '''''save the date to the wav file'''      wf = wave.open(filename, 'wb')      wf.setnchannels(channels)      wf.setsampwidth(sampwidth)      wf.setframerate(framerate)      wf.writeframes(b"".join(data))      wf.close()        def my_button(root,label_text,button_text,button_func):        '''''''function of creat label and button'''        #label details        label = Label(root)        label['text'] = label_text        label.pack()        #label details        button = Button(root)        button['text'] = button_text        button['command'] = button_func        button.pack()            def record_wave():      #open the input of wave      pa = PyAudio()      stream = pa.open(format = paInt16, channels = 1,                     rate = framerate, input = True,                     frames_per_buffer = NUM_SAMPLES)      save_buffer = []      count = 0      while count < TIME:      #read NUM_SAMPLES sampling data          string_audio_data = stream.read(NUM_SAMPLES)          save_buffer.append(string_audio_data)          count += 1          print ('.')            #filename = datetime.now().strftime("%Y-%m-%d_%H_%M_%S")+".wav"      filename = "test.wav"      save_wave_file(filename, save_buffer)      save_buffer = []      print (filename, "saved")        def record_wav_first():      record_wave()   
